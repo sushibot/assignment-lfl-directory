7fa484331e564c9656f3da1f8df0e0ef541e2992
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2022-01-07 01:38:41 -1000

    refactor: made sidebar sticky and creating components

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const person_add_icon = "./assets/icons/person_add.svg";
 const search_icon = "./assets/icons/search.svg";
 const delete_icon = "./assets/icons/delete.svg";
 const edit_icon = "./assets/icons/edit.svg";
+const done_icon = "./assets/icons/done.svg";
 
 $("#directory-list-page").on("click", directory_list_page);
 $("#directory-add-person-page").on("click", directory_add_person_page);
@@ -13,20 +14,19 @@ $("#directory-delete-person-page").on("click", directory_delete_person_page);
 $("#directory-update-person-page").on("click", directory_update_person_page);
 
 function init() {
-  get_localstorage();
   return directory_list_page();
 }
 
-function update_localstorage() {
-  localStorage.setItem("directory", JSON.stringify(employeeList));
-}
+// function update_localstorage() {
+//   localStorage.setItem("directory", JSON.stringify(employeeList));
+// }
 
-function get_localstorage() {
-  const list = localStorage.getItem("directory");
-  const length = localStorage.length;
-  if (!length) return;
-  employeeList = JSON.parse(list);
-}
+// function get_localstorage() {
+//   const list = localStorage.getItem("directory");
+//   const length = localStorage.length;
+//   if (!length) return;
+//   employeeList = JSON.parse(list);
+// }
 
 function reset_view() {
   directory_container.textContent = "";
@@ -35,8 +35,10 @@ function reset_view() {
 /***** PAGE NAVIGATION OPERATIONS *****/
 function directory_list_page() {
   reset_view();
+  const ul = document.createElement("ul");
+  ul.classList.add("directory-list-container");
   employeeList.forEach((employee) => {
-    const div = document.createElement("div");
+    const li = document.createElement("li");
     const name = document.createElement("label");
     const office_num = document.createElement("p");
     const phone_num = document.createElement("p");
@@ -45,12 +47,13 @@ function directory_list_page() {
     office_num.innerText = employee.officeNum;
     phone_num.innerText = employee.phoneNum;
 
-    div.classList.add("card");
-    div.appendChild(name);
-    div.appendChild(office_num);
-    div.appendChild(phone_num);
-    directory_container.appendChild(div);
+    li.classList.add("card");
+    li.appendChild(name);
+    li.appendChild(office_num);
+    li.appendChild(phone_num);
+    ul.appendChild(li);
   });
+  directory_container.appendChild(ul);
 }
 
 function directory_add_person_page() {
@@ -84,6 +87,26 @@ function directory_add_person_page() {
   form.id = "insert-person-form";
   directory_container.appendChild(form);
   button.addEventListener("click", () => insert_person());
+
+  const ul = document.createElement("ul");
+  ul.classList.add("directory-list-container");
+  employeeList.forEach((employee) => {
+    const li = document.createElement("li");
+    const name = document.createElement("label");
+    const office_num = document.createElement("p");
+    const phone_num = document.createElement("p");
+
+    name.innerText = employee.name;
+    office_num.innerText = employee.officeNum;
+    phone_num.innerText = employee.phoneNum;
+
+    li.classList.add("card");
+    li.appendChild(name);
+    li.appendChild(office_num);
+    li.appendChild(phone_num);
+    ul.appendChild(li);
+  });
+  directory_container.appendChild(ul);
 }
 
 function directory_verify_person_page() {
@@ -104,7 +127,30 @@ function directory_verify_person_page() {
   form.name = "verify-person";
 
   directory_container.appendChild(form);
-  button.addEventListener("click", () => find_person());
+  button.addEventListener("click", () => {
+    const employee = find_person(form.name);
+    verify_person_view(employee);
+  });
+
+  const ul = document.createElement("ul");
+  ul.classList.add("directory-list-container");
+  employeeList.forEach((employee) => {
+    const li = document.createElement("li");
+    const name = document.createElement("label");
+    const office_num = document.createElement("p");
+    const phone_num = document.createElement("p");
+
+    name.innerText = employee.name;
+    office_num.innerText = employee.officeNum;
+    phone_num.innerText = employee.phoneNum;
+
+    li.classList.add("card");
+    li.appendChild(name);
+    li.appendChild(office_num);
+    li.appendChild(phone_num);
+    ul.appendChild(li);
+  });
+  directory_container.appendChild(ul);
 }
 
 function directory_delete_person_page() {
@@ -127,6 +173,26 @@ function directory_delete_person_page() {
 
   directory_container.appendChild(form);
   button.addEventListener("click", () => delete_person());
+
+  const ul = document.createElement("ul");
+  ul.classList.add("directory-list-container");
+  employeeList.forEach((employee) => {
+    const li = document.createElement("li");
+    const name = document.createElement("label");
+    const office_num = document.createElement("p");
+    const phone_num = document.createElement("p");
+
+    name.innerText = employee.name;
+    office_num.innerText = employee.officeNum;
+    phone_num.innerText = employee.phoneNum;
+
+    li.classList.add("card");
+    li.appendChild(name);
+    li.appendChild(office_num);
+    li.appendChild(phone_num);
+    ul.appendChild(li);
+  });
+  directory_container.appendChild(ul);
 }
 
 function directory_update_person_page() {
@@ -146,11 +212,18 @@ function directory_update_person_page() {
   form.classList.add("directory-form-container");
   form.name = "update-person";
   form.id = "update-person-form";
-  directory_container.appendChild(form);
-  button.addEventListener("click", update_person);
 
+  directory_container.appendChild(form);
+
+  button.addEventListener("click", () => {
+    const employee = find_person(form.name);
+    update_employee_form_view(employee);
+  });
+
+  const ul = document.createElement("ul");
+  ul.classList.add("directory-list-container");
   employeeList.forEach((employee) => {
-    const div = document.createElement("div");
+    const li = document.createElement("li");
     const name = document.createElement("label");
     const office_num = document.createElement("p");
     const phone_num = document.createElement("p");
@@ -159,15 +232,16 @@ function directory_update_person_page() {
     office_num.innerText = employee.officeNum;
     phone_num.innerText = employee.phoneNum;
 
-    div.classList.add("card");
-    div.appendChild(name);
-    div.appendChild(office_num);
-    div.appendChild(phone_num);
-    directory_container.appendChild(div);
+    li.classList.add("card");
+    li.appendChild(name);
+    li.appendChild(office_num);
+    li.appendChild(phone_num);
+    ul.appendChild(li);
   });
+  directory_container.appendChild(ul);
 }
 
-function update_employee_form_view() {
+function update_employee_form_view(employee) {
   const update_form = document.getElementById("update-person-form");
 
   const form = document.createElement("form");
@@ -177,7 +251,7 @@ function update_employee_form_view() {
   const button = document.createElement("button");
   const button_icon = document.createElement("img");
 
-  button_icon.setAttribute("src", person_add_icon);
+  button_icon.setAttribute("src", done_icon);
   button.appendChild(button_icon);
   button.type = "button";
 
@@ -185,10 +259,16 @@ function update_employee_form_view() {
 
   input_name.classList.add("form-input");
   input_name.name = "name";
+  input_name.value = employee.name;
+  input_name.readOnly = true;
+
   input_office_num.classList.add("form-input");
   input_office_num.name = "officeNum";
+  input_office_num.value = employee.officeNum;
+
   input_phone_num.classList.add("form-input");
   input_phone_num.name = "phoneNum";
+  input_phone_num.value = employee.phoneNum;
 
   form.appendChild(input_name);
   form.appendChild(input_office_num);
@@ -217,6 +297,17 @@ function verify_person_view(employee) {
   directory_container.appendChild(div);
 }
 
+/***** COMPONENTS *****/
+
+function input_component({ name, value = "", read_only = false } = {}) {
+  const input = document.createElement("input");
+  if (value) {
+    input.value = value;
+  }
+  input.readOnly = read_only;
+  input.classList.add("form-input");
+  return input;
+}
 /***** CRUD OPERATIONS *****/
 function update_person() {
   const forms = document.forms["update-person"].getElementsByTagName("input");
@@ -239,13 +330,15 @@ function insert_person() {
     phoneNum: "",
   };
   inputs.forEach((input) => {
-    if (input.value) {
+    console.log(input.value);
+    if (input.value !== "") {
       new_person[input.name] = input.value;
     }
   });
   employeeList.push(new_person);
-  update_localstorage();
+
   alert(`Succesfully added ${new_person.name.toUpperCase()}!`);
+  directory_add_person_page();
 }
 
 function delete_person() {
@@ -260,14 +353,16 @@ function delete_person() {
   );
   if (found_employee_index > -1) {
     employeeList.splice(found_employee_index, 1);
-    return alert(`Succesfully deleted ${input_value.toUpperCase()}!`);
+    alert(`Succesfully deleted ${input_value.toUpperCase()}!`);
   } else {
     alert(`No record found for ${input_value.toUpperCase()}!`);
   }
+
+  directory_delete_person_page();
 }
 
-function find_person() {
-  const forms = document.forms["verify-person"].getElementsByTagName("input");
+function find_person(form_name) {
+  const forms = document.forms[form_name].getElementsByTagName("input");
   const inputs = Array.from(forms);
   const input_value = inputs[0].value.toLowerCase();
 
@@ -279,7 +374,7 @@ function find_person() {
   if (found_employee_index > -1) {
     const found_employee = employeeList[found_employee_index];
 
-    return verify_person_view(found_employee);
+    return found_employee;
   } else {
     return alert(
       `No employee found under the name ${input_value.toUpperCase()}`
