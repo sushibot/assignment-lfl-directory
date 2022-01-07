45aa8a60266bc8fbb38ac30e590427f0f6425fe9
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2022-01-06 23:24:16 -1000

    feat: add update function

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Your work here
 const directory_container = document.getElementById("directory");
+
 const person_add_icon = "./assets/icons/person_add.svg";
 const search_icon = "./assets/icons/search.svg";
 const delete_icon = "./assets/icons/delete.svg";
+const edit_icon = "./assets/icons/edit.svg";
 
 $("#directory-list-page").on("click", directory_list_page);
 $("#directory-add-person-page").on("click", directory_add_person_page);
 $("#directory-verify-person-page").on("click", directory_verify_person_page);
 $("#directory-delete-person-page").on("click", directory_delete_person_page);
+$("#directory-update-person-page").on("click", directory_update_person_page);
 
 function init() {
   return directory_list_page();
@@ -117,6 +120,75 @@ function directory_delete_person_page() {
   button.addEventListener("click", () => delete_person());
 }
 
+function directory_update_person_page() {
+  reset_view();
+  const form = document.createElement("form");
+  const input_name = document.createElement("input");
+  const button = document.createElement("button");
+  const button_icon = document.createElement("img");
+
+  button_icon.setAttribute("src", edit_icon);
+  button.appendChild(button_icon);
+  button.type = "button";
+  input_name.classList.add("form-input");
+
+  form.appendChild(input_name);
+  form.appendChild(button);
+  form.classList.add("directory-form-container");
+  form.name = "update-person";
+  form.id = "update-person-form";
+  directory_container.appendChild(form);
+  button.addEventListener("click", update_person);
+
+  employeeList.forEach((employee) => {
+    const div = document.createElement("div");
+    const name = document.createElement("label");
+    const office_num = document.createElement("p");
+    const phone_num = document.createElement("p");
+
+    name.innerText = employee.name;
+    office_num.innerText = employee.officeNum;
+    phone_num.innerText = employee.phoneNum;
+
+    div.classList.add("card");
+    div.appendChild(name);
+    div.appendChild(office_num);
+    div.appendChild(phone_num);
+    directory_container.appendChild(div);
+  });
+}
+function update_employee_form_view() {
+  const update_form = document.getElementById("update-person-form");
+
+  const form = document.createElement("form");
+  const input_name = document.createElement("input");
+  const input_office_num = document.createElement("input");
+  const input_phone_num = document.createElement("input");
+  const button = document.createElement("button");
+  const button_icon = document.createElement("img");
+
+  button_icon.setAttribute("src", person_add_icon);
+  button.appendChild(button_icon);
+  button.type = "button";
+
+  form.classList.add("directory-form-container");
+
+  input_name.classList.add("form-input");
+  input_name.name = "name";
+  input_office_num.classList.add("form-input");
+  input_office_num.name = "officeNum";
+  input_phone_num.classList.add("form-input");
+  input_phone_num.name = "phoneNum";
+
+  form.appendChild(input_name);
+  form.appendChild(input_office_num);
+  form.appendChild(input_phone_num);
+  form.appendChild(button);
+  form.name = "insert-person";
+  form.id = "insert-person-form";
+  update_form.appendChild(form);
+  button.addEventListener("click", () => update_person);
+}
 function verify_person_view(employee) {
   const div = document.createElement("div");
   const name = document.createElement("label");
@@ -132,6 +204,18 @@ function verify_person_view(employee) {
   div.appendChild(office_num);
   div.appendChild(phone_num);
   directory_container.appendChild(div);
+}
+
+function update_person() {
+  const forms = document.forms["update-person"].getElementsByTagName("input");
+  const inputs = Array.from(forms);
+  const input_value = inputs[0].value.toLowerCase();
+  if (!input_value) return alert("Please type in the name you wish to update.");
+  const employee = employeeList.find(
+    (employee) => employee.name.toLowerCase() === input_value
+  );
+  if (!employee) return alert(`No matching record found for ${input_value}`);
+  update_employee_form_view();
 }
 
 function insert_person() {
@@ -157,8 +241,7 @@ function delete_person() {
   const inputs = Array.from(forms);
   const input_value = inputs[0].value.toLowerCase();
 
-  if (!input_value)
-    return alert("Please fill in the person you wish to delete.");
+  if (!input_value) return alert("Please type in the name you wish to delete.");
 
   employeeList.forEach((employee, index) => {
     const name = employee.name.toLowerCase();
@@ -177,8 +260,7 @@ function find_person() {
   const input_value = inputs[0].value.toLowerCase();
   let result = false;
   let found_employee = "";
-  if (!input_value)
-    return alert("Please fill in the person you wish to verify.");
+  if (!input_value) return alert("Please type in the name you wish to verify.");
   employeeList.forEach((employee, index) => {
     const name = employee.name.toLowerCase();
 
